cf7588be44ffc28f14ed07266be65bbe34ef6353
hantsy <hantsy@gmail.com>
2022-09-24 00:55:15 +0800

    chore: clean cdes

--- record-pattern/src/main/java/com/example/demo/RecordPatternExample.java
+++ record-pattern/src/main/java/com/example/demo/RecordPatternExample.java
@@ -13,6 +13,10 @@ public class RecordPatternExample {
 
     public static final void main(String args[]) {
         printPerson(new Person("Hantsy", "Bai"));
+        
+        printShapeArea(new Circle(1.2));
+        printShapeArea(new Square(1.2));
+        printShapeArea(new Rectangle(1.0, 2.0));
     }
 
     static void printPerson(Object object) {
@@ -21,7 +25,45 @@ public class RecordPatternExample {
             return;
         }
     }
+    
+    static void printShapeArea(Shape s) {
+        var shape =  switch(s) {
+            case Circle(Double r) -> "Circle with r:" + r;
+            case Square(Double x) -> "Square with x:" + x;
+            case Rectangle(Double x, Double y) -> "Rectangle with x: "+x+ " and y:"+y;
+            default -> "Unkown";
+        };
+        System.out.println(shape + ", area:"+ s.area());
+    }
 
 }
 
 record Person(String firstName, String lastName) {}
+
+sealed interface Shape permits Circle, Square, Rectangle{
+    Double area();
+}
+
+record Circle(Double r) implements Shape{
+
+    @Override
+    public Double area() {
+        return r * r * 3.14;
+    }
+}
+
+record Square(Double x) implements Shape{
+
+    @Override
+    public Double area() {
+        return x * x;
+    }
+}
+
+record Rectangle(Double x, Double y) implements Shape{
+
+    @Override
+    public Double area() {
+        return x * y;
+    }
+}
